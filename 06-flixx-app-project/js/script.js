b71fdc4ae23dca55bfe9fb6faf42b0c21ee8e297
RU063 <rashedinislam.06@gmail.com>
2024-05-21 16:06:16 +0600

    loading spinner & popular tv shows section

--- 06-flixx-app-project/js/script.js
+++ 06-flixx-app-project/js/script.js
@@ -13,9 +13,10 @@ function highlightActiveLink() {
   });
 }
 
+// Display 20 most popular movies
 async function displayPopularMovie() {
+  loadSpinner(true)
   const {results} = await fetchAPIData('movie/popular');
-console.log(results)
 
   results.forEach(movie => {
     const div = document.createElement('div');
@@ -45,8 +46,46 @@ console.log(results)
         `;
 
     document.querySelector('#popular-movies').appendChild(div);
-})
-  
+
+    loadSpinner(false)
+}) 
+}
+
+// Display 20 most popular tv shows
+async function displayPopularShows() {
+  loadSpinner(true)
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+  loadSpinner(false)
 }
 
 // Fetch data from TMDB API
@@ -63,6 +102,12 @@ async function fetchAPIData(endpoint) {
   return data;
 }
 
+function loadSpinner(isShowing) {
+  isShowing
+    ? document.querySelector('.spinner').classList.add('show')
+    : document.querySelector('.spinner').classList.remove('show');
+}
+
 // init app
   function init() {
   switch (global.currentPage) {
@@ -72,7 +117,7 @@ async function fetchAPIData(endpoint) {
       displayPopularMovie()
       break;
     case '/shows.html':
-      console.log('Shows');
+   displayPopularShows()
       break;
     case '/movie-details.html':
       console.log('Movie Details');
